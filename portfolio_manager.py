e5c9e9288d36a3921b61e4f3f1f189d4bec9ae96
Roi <roi@github.com>
2020-09-12 17:35:38 +0200

    Update portfolio_manager.py
    Adding comments https://news.ycombinator.com/item?id=24429243

--- portfolio_manager.py
+++ portfolio_manager.py
@@ -10,9 +10,21 @@ warnings.filterwarnings("ignore")
 
 """
 Sample run:
-python portfolio_manager.py --is_test 1 --future_bars 90 --data_granularity_minutes 3600 --history_to_use all --apply_noise_filtering 1 --market_index QQQ --only_long 1 --eigen_portfolio_number 3 --save_plot False
+python3 portfolio_manager.py --is_test 1 --future_bars 540 --data_granularity_minutes 3600 --history_to_use all --apply_noise_filtering 1 --market_index SPY --only_long 1 --eigen_portfolio_number 3 --stocks_file_path stocks/test.txt
 """
 
+"""
+On interpreting portfolio outputs:
+
+https://news.ycombinator.com/item?id=24429243:
+	
+hydershykh 2 days ago | parent | favorite | on: Show HN: Eiten – open-source tool for portfolio op...
+
+So the negative weights are just ignored during the forward and back tests. They are there just to show you
+the raw portfolios without any filtering. As for the other question, the weights are just proportions of your 
+money that you should put in each stock. If a weight is negative, that means just short with that proportion. 
+You can simply normalize the weights to sum up to one if it's harder to read them without them being normalized.
+"""
 def main():
 
     argParser = argparse.ArgumentParser()
